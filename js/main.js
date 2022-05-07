5c907692c01c94aad9883db7c0aa56b02c7aefa4
CapFrostyMug <nickolas7159@gmail.com>
2022-05-07 18:34:13 +0500

    Homework (lesson 2)
    Что нового:
    
    - созданы первые Классы: отныне контент на странице генерируется с их помощью;
    - для класса Product реализован метод подсчета стоимости всех товаров.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,72 @@
-const data = [
-    {title: "MSI NVIDIA GeForce 210", id: 1, price: 4190},
-    {title: "Palit NVIDIA GeForce GT 710", id: 2, price: 4390},
-    {title: "GIGABYTE NVIDIA GeForce GT 730", id: 3, price: 6990},
-    {title: "Palit NVIDIA GeForce GT 1030", id: 4, price: 7890},
-    {title: "ASUS AMD Radeon RX 6500XT", id: 5, price: 33990},
-    {title: "GIGABYTE NVIDIA GeForce GTX 1660", id: 6, price: 43990},
-];
+class Products {
 
-const renderProduct = (title, id, price) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <span hidden id="${id}"></span>
-            </div>`
-};
+    data = [];
+    products = [];
+    container = null;
+    totalPrice = 0;
 
-const render = (products) => {
-    const productsList = products.map(item => renderProduct(item.title, item.id, item.price, item.id)).join('');
-    document.querySelector('.products').insertAdjacentHTML('beforeend', productsList);
+    constructor(selector) {
+        this.container = document.querySelector(selector);
+        this._fetchData();
+        this._render();
+        this.getSum(this.data);
+    };
+
+    /*init() {
+        this._fetchData();
+        this._render();
+    };*/
+
+    getSum(products) {
+        for (let product of products) {
+            this.totalPrice += product.price;
+        }
+        console.log(this.totalPrice);
+    };
+
+    _fetchData() {
+        this.data = [
+            {title: "MSI NVIDIA GeForce 210", id: 1, price: 4190},
+            {title: "Palit NVIDIA GeForce GT 710", id: 2, price: 4390},
+            {title: "GIGABYTE NVIDIA GeForce GT 730", id: 3, price: 6990},
+            {title: "Palit NVIDIA GeForce GT 1030", id: 4, price: 7890},
+            {title: "ASUS AMD Radeon RX 6500XT", id: 5, price: 33990},
+            {title: "GIGABYTE NVIDIA GeForce GTX 1660", id: 6, price: 43990},
+        ]
+    };
+
+    _render() {
+        for (let data of this.data) {
+            const productItem = new ProductItem(data);
+            this.products.push(productItem);
+            this.container.insertAdjacentHTML('beforeend', productItem.render());
+        }
+    };
 }
 
-render(data);
+class ProductItem {
+
+    title = "";
+    price = 0;
+    id = 0;
+    img = "";
+
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
+        ({title: this.title, price: this.price, id: this.id} = product);
+        this.img = img;
+    };
+
+    render() {
+        return `<div class="product-item">
+                    <img src="${this.img}" alt="${this.title}">
+                    <div class="desc">
+                        <h3>${this.title}</h3>
+                        <p>${this.price}</p>
+                        <button class="buy-btn" data-id="${this.id}">В корзину</button>
+                    </div>
+                </div>`
+    }
+}
+
+const list = new Products('.products');
+// list.init();

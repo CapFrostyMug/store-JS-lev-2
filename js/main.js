c698e09968b25e6635c5ed16b6afc217c6b44a94
CapFrostyMug <nickolas7159@gmail.com>
2022-05-12 12:58:28 +0500

    Homework (lesson 3)
    Что нового:
    
    - реализована корзина

--- js/main.js
+++ js/main.js
@@ -1,72 +1,235 @@
-class Products {
+/**
+ * Базовый Класс Item
+ */
 
-    data = [];
-    products = [];
-    container = null;
-    totalPrice = 0;
+class Item {
 
-    constructor(selector) {
-        this.container = document.querySelector(selector);
-        this._fetchData();
-        this._render();
-        this.getSum(this.data);
+    product_name = "";
+    price = 0;
+    id_product = 0;
+    img = "";
+    rendered = false;
+
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
+        ({product_name: this.product_name, price: this.price, id_product: this.id_product} = product);
+        this.img = img;
     };
 
-    /*init() {
-        this._fetchData();
+    render() {
+        this.rendered = true;
+        return `<div class="product-item">
+                    <img src="${this.img}" alt="${this.product_name}">
+                    <div class="desc">
+                        <h3>${this.product_name}</h3>
+                        <p>${this.price}</p>
+                        <button class="buy-btn" data-id="${this.id_product}">В корзину</button>
+                    </div>
+                </div>`;
+    };
+
+}
+
+/**
+ * Класс ProductItem
+ */
+
+class ProductItem extends Item {
+}
+
+/**
+ * Класс CartItem
+ */
+
+class CartItem extends Item {
+
+    quantity = 0;
+
+    constructor(product, img = 'https://via.placeholder.com/50x50') {
+        super(product, img);
+        this.quantity = product.quantity;
+    };
+
+    changeQuantity(count) {
+        this.quantity += count;
+        this._updateItem();
+    };
+
+    removeMarkup() {
+        document.querySelector(`.cart-item[data-id="${this.id_product}"]`).remove();
+    };
+
+    render() {
+        this.rendered = true;
+        return `<div class="cart-item" data-id="${this.id_product}">
+                    <div class="product-bio">
+                        <img src="${this.img}" alt="Some image">
+                        <div class="product-desc">
+                        <p class="product-title">${this.product_name}</p>
+                        <p class="product-quantity">Quantity: ${this.quantity}</p>
+                        <p class="product-single-price">$${this.price} each</p>
+                        </div>
+                    </div>
+                    <div class="right-block">
+                        <p class="product-price">$${this.quantity * this.price}</p>
+                        <button class="del-btn" data-id="${this.id_product}">&times;</button>
+                    </div>
+                </div>`;
+    };
+
+    _updateItem() {
+        const block = document.querySelector(`.cart-item[data-id="${this.id_product}"]`);
+        block.querySelector('.product-quantity').textContent = `Quantity: ${this.quantity}`;
+        block.querySelector('.product-price').textContent = `$${this.quantity * this.price}`;
+    };
+
+}
+
+/**
+ * Базовый Класс List
+ */
+
+class List {
+
+    static itemsMap = {
+        Products: ProductItem,
+        Cart: CartItem,
+    };
+
+    static API = `https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses`;
+
+    products = [];
+    container = null;
+    url = '';
+
+    constructor(selector, url) {
+        this.container = document.querySelector(selector);
+        this.url = url;
+        this._init();
+    };
+
+    getJson(url) {
+        return fetch(url ? url : `${List.API + this.url}`)
+            .then(result => result.json());
+    };
+
+    handleData(data) {
+        for (let item of data) {
+            this.products.push(new List.itemsMap[this.constructor.name](item));
+        }
         this._render();
-    };*/
+    };
+
+    getItem(id) {
+        return this.products.find(item => item.id_product === id);
+    };
 
     getSum(products) {
         for (let product of products) {
             this.totalPrice += product.price;
         }
-        console.log(this.totalPrice);
+        return this.totalPrice;
     };
 
-    _fetchData() {
-        this.data = [
-            {title: "MSI NVIDIA GeForce 210", id: 1, price: 4190},
-            {title: "Palit NVIDIA GeForce GT 710", id: 2, price: 4390},
-            {title: "GIGABYTE NVIDIA GeForce GT 730", id: 3, price: 6990},
-            {title: "Palit NVIDIA GeForce GT 1030", id: 4, price: 7890},
-            {title: "ASUS AMD Radeon RX 6500XT", id: 5, price: 33990},
-            {title: "GIGABYTE NVIDIA GeForce GTX 1660", id: 6, price: 43990},
-        ]
+    _init() {
     };
 
     _render() {
-        for (let data of this.data) {
-            const productItem = new ProductItem(data);
-            this.products.push(productItem);
-            this.container.insertAdjacentHTML('beforeend', productItem.render());
+        for (let product of this.products) {
+            if (product.rendered) {
+                continue;
+            }
+            this.container.insertAdjacentHTML('beforeend', product.render());
         }
     };
+
 }
 
-class ProductItem {
+/**
+ * Класс ProductsList
+ */
 
-    title = "";
-    price = 0;
-    id = 0;
-    img = "";
+class Products extends List {
 
-    constructor(product, img = 'https://via.placeholder.com/200x150') {
-        ({title: this.title, price: this.price, id: this.id} = product);
-        this.img = img;
+    cart = null;
+
+    constructor(cart, container = '.products', url = '/catalogData.json') {
+        super(container, url);
+        this.cart = cart;
+        this.getJson()
+            .then(data => this.handleData(data));
     };
 
-    render() {
-        return `<div class="product-item">
-                    <img src="${this.img}" alt="${this.title}">
-                    <div class="desc">
-                        <h3>${this.title}</h3>
-                        <p>${this.price}</p>
-                        <button class="buy-btn" data-id="${this.id}">В корзину</button>
-                    </div>
-                </div>`
-    }
+    _init() {
+        this.container.addEventListener('click', event => {
+            if (event.target.classList.contains('buy-btn')) {
+                const id = +event.target.dataset['id'];
+                this.cart.addProduct(this.getItem(id));
+            }
+        });
+    };
+
 }
 
-const list = new Products('.products');
-// list.init();
+/**
+ * Класс Cart
+ */
+
+class Cart extends List {
+
+    constructor(container = '.cart-block', url = '/getBasket.json') {
+        super(container, url);
+        this.getJson()
+            .then(data => this.handleData(data.contents));
+    };
+
+    addProduct(product) {
+        this.getJson(`${List.API}/addToBasket.json`)
+            .then(data => {
+                if (data.result) {
+                    let find = this.products.find(item => item.id_product === product.id_product);
+                    if (find) {
+                        find.changeQuantity(1);
+                        return;
+                    }
+
+                    let prod = Object.assign({quantity: 1}, product);
+                    this.handleData([prod]);
+                } else {
+                    console.log('Error');
+                }
+            });
+    };
+
+    removeProduct(product) {
+        this.getJson(`${List.API}/deleteFromBasket.json`)
+            .then(data => {
+                if (data.result) {
+                    if (product.quantity > 1) {
+                        product.changeQuantity(-1);
+                        return;
+                    }
+                    this.products.splice(this.products.indexOf(product), 1);
+                    product.removeMarkup();
+                } else {
+                    console.log('Error');
+                }
+            });
+    };
+
+    _init() {
+        this.container.addEventListener('click', event => {
+            if (event.target.classList.contains('del-btn')) {
+                const id = +event.target.dataset['id'];
+                this.removeProduct(this.getItem(id));
+            }
+        });
+
+        document.querySelector('.btn-cart').addEventListener('click', () => {
+            this.container.classList.toggle('invisible');
+        });
+    };
+
+}
+
+const cart = new Cart();
+const list = new Products(cart);
